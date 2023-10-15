0e1dd7dec5334e4d447d51e7db0a10596061cab7
SpindlySpider <kaihrmt@gmail.com>
2023-10-15 16:45:32 +0100

    palindrome

---
+++ week3/5.py
@@ -0,0 +1,20 @@
+def palindrome(string:str)->bool:
+    if len(string) <= 1:
+        return False
+    string_list = []
+    for i in range(len(string)):
+        string_list.append(string[i])
+    reversed = []
+    for i in range(len(string)):
+        reversed.append(string[len(string)-1-i])
+    same_char = True
+    for i in range(len(string)):
+        if string_list[i] == reversed[i]:
+            pass
+        else:
+            same_char = False
+    return same_char
+    
+    
+print(palindrome("aa"))
+    
